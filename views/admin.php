1596c581c087422c3434c818cf6b23463f932eb5
J. Aaron Eaton <aaron@channeleaton.com>
2013-06-11 16:56:33 -0500

    Added a little styling.

--- views/admin.php
+++ views/admin.php
@@ -4,7 +4,9 @@
 	<h2><?php _e( 'Site Data', 'agriflex' ); ?></h2>
 	
 	<form method="post" name="site-data-exporter" action="<?php echo plugin_dir_url( __FILE__ ) . 'export.php'; ?>">
-		<p class="submit"><input type="submit" name="Download" value="Download" /></p>
+		<h3>Download all site data: </h3>
+		<p class="submit">
+			<input class="button button-primary" type="submit" name="Download" value="Download" /></p>
 	</form>
 
 	
