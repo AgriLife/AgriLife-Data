cc5528aca8472de85639835fe441eabf8155bc36
Zachary Watkins <watkinza@gmail.com>
2017-01-17 14:50:00 -0600

    Fix problem with dependency detection which was affecting txmn

--- lib/AgencyAffiliation.php
+++ lib/AgencyAffiliation.php
@@ -8,48 +8,61 @@
 class AgencyAffiliation {
 
   public function __construct() {
-    
+
     // Do not provide this menu for AgriFlex2 themes
     if ( wp_get_theme()->Name != 'AgriFlex2' ) {
 
-      if( !class_exists('acf') || !function_exists('acf_add_options_sub_page') ){
+      // Add admin page
+      add_action( 'admin_menu', function(){
 
-        add_action( 'admin_notices', array( $this, 'errormsg' ) );
+        if( !class_exists('acf') ){
 
-      } else {
+          add_action( 'admin_notices', array( $this, 'errormsg_acf' ) );
 
-        // Add fields
-        require_once( AGDATA_DIR_PATH . '/fields/agency-affiliation.php' );
+        } else {
 
-        // Add admin page
-        add_action( 'admin_menu', function(){
+          // Add fields
+          require_once( AGDATA_DIR_PATH . '/fields/agency-affiliation.php' );
 
-          acf_add_options_sub_page( array(
-            'page_title'    => 'Agency Affiliation',
-            'menu_title'    => 'Agency Affiliation',
-            'menu_slug'     => 'agrilifedata-siteaffiliation',
-            'capability'    => 'manage_options',
-            'parent_slug'   => 'options-general.php'
-          ) );
+          try {
 
-        } );
+            acf_add_options_sub_page( array(
+              'page_title'    => 'Agency Affiliation',
+              'menu_title'    => 'Agency Affiliation',
+              'menu_slug'     => 'agrilifedata-siteaffiliation',
+              'capability'    => 'manage_options',
+              'parent_slug'   => 'options-general.php'
+            ) );
 
-      }
+          } catch (Exception $e) {
+
+            add_action( 'admin_notices', array( $this, 'errormsg_acf_add_options_sub_page' ) );
+
+          }
+
+        }
+
+      } );
 
     }
 
   }
 
-  public function errormsg() {
-
-    if( !class_exists('acf') )
-      $msg = 'Advanced Custom Fields is not installed';
-    else if( !function_exists('acf_add_options_sub_page') )
-      $msg = 'the function "acf_add_options_sub_page" is not defined';
+  public function errormsg_acf() {
 
     ?>
     <div class="error notice">
-      <p>The Agency Affiliation menu is unavailable because <?php echo $msg; ?>. Either install the Advanced Custom Fields Pro plugin version 5.0.0 or above, or Advanced Custom Fields plugin and the Options Page Add-on plugin.</p>
+      <p>The Agency Affiliation menu is unavailable because Advanced Custom Fields is not installed. Either install the Advanced Custom Fields Pro plugin version 5.0.0 or above, or Advanced Custom Fields plugin and the Options Page Add-on plugin.</p>
+    </div>
+    <?php
+
+  }
+
+  public function errormsg_acf_add_options_sub_page() {
+
+    ?>
+    <div class="error notice">
+      <p>The Agency Affiliation menu is unavailable because the function "acf_add_options_sub_page" is not defined. Either install the Advanced Custom Fields Pro plugin version 5.0.0 or above, or Advanced Custom Fields plugin and the Options Page Add-on plugin.</p>
     </div>
     <?php
 
